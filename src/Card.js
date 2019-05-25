8480164e7868b6baed6367088194053d8439507d
Emmanuel <gramme.oe@gmail.com>
2019-05-25 02:10:19 +0100

    first

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./css/Card.css";
+
+const Card = () => {
+	return (
+		<div className='card-container'>
+			<img alt='' src='https://www.robohash.org/ante?400x400' />
+			<div>
+				<div />
+				<h2>Emmanuel Zyphron</h2>
+				<p>I am a master healer terridophyta</p>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
