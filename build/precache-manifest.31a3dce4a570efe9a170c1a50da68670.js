2b2fc16669e2e68ee9ab040272beacc77b6d3fd4
Emmanuel <gramme.oe@gmail.com>
2019-05-27 05:53:22 +0100

    Production 2

---
+++ build/precache-manifest.31a3dce4a570efe9a170c1a50da68670.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3d5ec3977bd8975abdd891fa53baf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66efc85a4449f0e68f07",
+    "url": "/static/css/main.5bafb5d6.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "66efc85a4449f0e68f07",
+    "url": "/static/js/main.43083e83.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
