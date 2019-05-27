fbd243c6da28b93f9c134752dd026ca585c928c4
Emmanuel <gramme.oe@gmail.com>
2019-05-27 06:12:08 +0100

    Updates

---
+++ precache-manifest.e480bf856561cfc3c9505bc1a9554a22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6c700ff727c48682e59d8233b1a97b",
+    "url": "/Robofamily/index.html"
+  },
+  {
+    "revision": "3186fc0267abf86a8b1a",
+    "url": "/Robofamily/static/css/main.5bafb5d6.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Robofamily/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3186fc0267abf86a8b1a",
+    "url": "/Robofamily/static/js/main.669a6c26.chunk.js"
+  },
+  {
+    "revision": "be468b3d9ce7dc809dae",
+    "url": "/Robofamily/static/js/runtime~main.37652d98.js"
+  }
+]);
