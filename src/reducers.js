e87aa27d8c1be3bb871506c49ce3ede2884e09ec
Emmanuel <gramme.oe@gmail.com>
2019-05-31 01:33:39 +0100

    redux step 1

---
+++ src/reducers.js
@@ -0,0 +1,14 @@
+import {
+	CHANGE_SEARCH_FIELD,
+	REQUEST_ROBOTS_PENDING,
+	REQUEST_ROBOTS_SUCCESS,
+	REQUEST_ROBOTS_FAILED
+} from "./constants";
+
+const initialStateSearch = {
+	searchField: ""
+};
+
+export const onSearchChange = (state = initialStateSearch, action = {}) => {
+    switch
+};
