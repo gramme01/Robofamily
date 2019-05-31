5a5157ba86596235f52fa527ba6404f7b8ed60ae
Emmanuel <gramme.oe@gmail.com>
2019-05-31 03:03:33 +0100

    Completed Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,41 +1,44 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 import "./css/App.css";
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchfield: ""
-		};
-	}
+import { setSearchField, requestRobots } from "../actions";
 
-	componentDidMount() {
-		fetch("//jsonplaceholder.typicode.com/users")
-			.then(response => response.json())
-			.then(users => this.setState({ robots: users }));
-	}
+const mapStateToProps = state => ({
+	searchField: state.searchRobots.searchField,
+	robots: state.requestRobots.robots,
+	isPending: state.requestRobots.isPending,
+	error: state.requestRobots.error
+});
 
-	onSearchChange = event => {
-		this.setState({ searchfield: event.target.value });
+const mapDispatchToProps = dispatch => {
+	return {
+		onSearchChange: event => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
 	};
+};
+
+class App extends Component {
+	componentDidMount() {
+		this.props.onRequestRobots();
+	}
 
 	render() {
-		const { robots, searchfield } = this.state;
+		const { robots, searchField, isPending, onSearchChange } = this.props;
 		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		});
 
-		return !robots.length ? (
+		return isPending ? (
 			<h1 className='tc'>Loading</h1>
 		) : (
 			<div className='container'>
 				<h1>ROBOFAMILY</h1>
-				<SearchBox searchChange={this.onSearchChange} />
+				<SearchBox searchChange={onSearchChange} />
 				<Scroll>
 					<ErrorBoundary>
 						<CardList robots={filteredRobots} />
@@ -46,4 +49,7 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(App);
